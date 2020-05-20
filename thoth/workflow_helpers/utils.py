dc342a6c1577438430f52ff899877d660126aba2
Francesco Murdaca <fmurdaca@redhat.com>
2020-05-20 19:48:33 +0200

    created method for workflow to switch between thoth integrations
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ thoth/workflow_helpers/utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# workflow-helpers
+# Copyright(C) 2020 Francesco Murdaca
+#
+# This program is free software: you can redistribute it and / or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""These are common scripts that can be reused."""
+
+
+THOTH_DOCUMENT_PATH = os.getenv("THOTH_DOCUMENT_PATH", None)
+
+
+def retrieve_adviser_document():
+    """Retrieve document ID."""
+    metadata = {}
+
+    if THOTH_DOCUMENT_PATH:
+
+        document = json.loads(THOTH_DOCUMENT_PATH)
+        cli_arguments = document["metadata"]["arguments"]["thoth-adviser"]
+        metadata = cli_arguments.get("metadata")
+
+    return metadata
