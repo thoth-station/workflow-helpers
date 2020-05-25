7e0df72ebdc3589f5f83826e150a0e2407abd39c
Francesco Murdaca <fmurdaca@redhat.com>
2020-05-25 19:17:49 +0200

    Add app and version
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

---
+++ app.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+#
+# This script is run by OpenShift's s2i. Here we guarantee that we run desired
+# command and debug level
+#
+
+if [ "$WORKFLOW_TASK" = "trigger_integration" ]
+then
+    exec pipenv run python3 select_thoth_integration main.py
+elif [ "$WORKFLOW_TASK" = "qeb_hwt" ]
+then
+    exec pipenv run python3 qeb_thamos_advise.py
+fi
--- qeb_thamos_advise.py
+++ qeb_thamos_advise.py
@@ -25,24 +25,23 @@ import logging
 from pathlib import Path
 
 from thamos.lib import advise_here
-from thamos.config import config
+from thamos.config import _Configuration
 from thoth.python.exceptions import FileLoadError
-from trigger_finished_webhook import trigger_finished_webhook
+from thamos.exceptions import NoRuntimeEnvironmentError
+from thoth.workflow_helpers.trigger_finished_webhook import trigger_finished_webhook
+from thoth.workflow_helpers.configuration import Configuration
 
 _LOGGER = logging.getLogger("qebhwt")
 
-_ORIGIN = os.getenv("ORIGIN")
-_GITHUB_EVENT_TYPE = os.getenv("GITHUB_EVENT_TYPE")
-_GITHUB_CHECK_RUN_ID = os.getenv("GITHUB_CHECK_RUN_ID")
-_GITHUB_INSTALLATION_ID = os.getenv("GITHUB_INSTALLATION_ID")
-_GITHUB_BASE_REPO_URL = os.getenv("GITHUB_BASE_REPO_URL")
-_THOTH_HOST = os.getenv("THOTH_HOST")
 
-
-def _create_message_config_file_error():
+def _create_message_config_file_error(no_file: bool):
     """Create message for config file error."""
-    message = "No .thoth.yaml was provided."
-    message += " Please add .thoth.yaml to your PR."
+    if no_file:
+        message = "No .thoth.yaml was provided."
+        message += " Please add .thoth.yaml to your PR."
+    else:
+        message = "No configuration for adviser in .thoth.yaml was provided."
+        message += " Please add configuration in .thoth.yaml to your PR."
 
     message += (
         "\n\nFor more information have a look at Qeb-Hwt README file:"
@@ -58,50 +57,59 @@ def _create_message_config_file_error():
 
         runtime_environments:
         - name: rhel:8
-        operating_system:
+          operating_system:
             name: rhel
             version: "8"
-        python_version: "3.6"
-        recommendation_type: stable
+          python_version: "3.6"
+          recommendation_type: stable
     """
     return message
 
 
 def qeb_hwt_thamos_advise():
     """Qeb-Hwt Thamos Advise Task."""
-    os.chdir("/mnt/inputs/artifacts/repository")
-    config.explicit_host = _THOTH_HOST
-    config.tls_verify = True
+    if not Configuration._REPO_PATH:
+        raise Exception(f"No path has been provided to REPO_PATH env variable.")
 
-    artifact_path = Path.cwd()
+    if not Path(Configuration._REPO_PATH).exists():
+        raise FileNotFoundError(f"Cannot find the file on this path: {Configuration._REPO_PATH}")
 
-    thoth_conf_file_path = artifact_path.joinpath(".thoth.yaml")
+    os.chdir(Configuration._REPO_PATH)
+    thoth_yaml_config = _Configuration()
 
-    if not thoth_conf_file_path.exists():
-        exception_message = _create_message_config_file_error()
-        trigger_finished_webhook(exception_message=exception_message, is_error=True)
-
-    with open(thoth_conf_file_path, "r") as yaml_file:
-        thoth_yaml = yaml.safe_load(yaml_file)
+    if not thoth_yaml_config.config_file_exists():
+        exception_message = _create_message_config_file_error(no_file=True)
+        trigger_finished_webhook(exception_message=exception_message, has_error=True)
+        return
 
     try:
         analysis_id = advise_here(
-            recommendation_type=thoth_yaml["runtime_environments"][0]["recommendation_type"],
-            runtime_environment=thoth_yaml["runtime_environments"][0],
             nowait=True,
-            github_event_type=_GITHUB_EVENT_TYPE,
-            github_check_run_id=_GITHUB_CHECK_RUN_ID,
-            github_installation_id=_GITHUB_INSTALLATION_ID,
-            github_base_repo_url=_GITHUB_BASE_REPO_URL,
-            origin=_ORIGIN,
-            source_type="GITHUB_APP",
+            github_event_type=Configuration._GITHUB_EVENT_TYPE,
+            github_check_run_id=Configuration._GITHUB_CHECK_RUN_ID,
+            github_installation_id=Configuration._GITHUB_INSTALLATION_ID,
+            github_base_repo_url=Configuration._GITHUB_BASE_REPO_URL,
+            origin=Configuration._ORIGIN,
+            # source_type="GITHUB_APP",
         )
+        _LOGGER.info("Successfully submitted thamos advise call.")
     except Exception as exception:
-        if isinstance(exception, (FileNotFoundError, FileLoadError, KeyError, ValueError)):
+        if isinstance(exception, (
+            NoRuntimeEnvironmentError,
+            FileNotFoundError,
+            FileLoadError,
+            KeyError,
+            ValueError
+            )
+        ):
             _LOGGER.debug(exception)
             exception_message = str(exception)
         else:
             _LOGGER.debug(json.loads(exception.body)["error"])
             exception_message = json.loads(exception.body)["error"]
 
-        trigger_finished_webhook(exception_message=exception_message, is_error=True)
+        trigger_finished_webhook(exception_message=exception_message, has_error=True)
+
+
+if __name__ == "__main__":
+    qeb_hwt_thamos_advise()
