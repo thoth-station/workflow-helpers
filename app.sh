7bbacf6208668ec70a0d66b2c3faf7eaec5a36e8
Francesco Murdaca <fmurdaca@redhat.com>
2020-07-24 18:54:47 +0200

    More robust app.sh
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- app.sh
+++ app.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env sh
 #
 # This script is run by OpenShift's s2i. Here we guarantee that we run desired
-# command and debug level
+# command
 #
 
-if [ "$THOTH_WORKFLOW_TASK" = "trigger_integration" ]
-then
+set -o nounset
+set -o errexit
+set -o errtrace
+set -o pipefail
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+THOTH_WORKFLOW_TASK=${THOTH_WORKFLOW_TASK:?'THOTH_WORKFLOW_TASK is not selected!'}
+
+if [ "$THOTH_WORKFLOW_TASK" = "trigger_integration" ]; then
     exec python3 select_thoth_integration.py
-elif [ "$THOTH_WORKFLOW_TASK" = "qeb_hwt" ]
-then
+elif [ "$THOTH_WORKFLOW_TASK" = "qeb_hwt" ]; then
     exec python3 qeb_thamos_advise.py
-elif [ "$THOTH_WORKFLOW_TASK" = "download_package"]
-then
+elif [ "$THOTH_WORKFLOW_TASK" = "download_package"]; then
     exec python3 download_py_package.py
 fi
