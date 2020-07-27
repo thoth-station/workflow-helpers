eee8bc44533e51f7eeeef7dbcbc4372e7f588770
Francesco Murdaca <fmurdaca@redhat.com>
2020-07-27 18:22:01 +0200

    Add space
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- app.sh
+++ app.sh
@@ -18,6 +18,6 @@ elif [ "$THOTH_WORKFLOW_TASK" = "qeb_hwt" ]; then
     exec python3 qeb_thamos_advise.py
 elif [ "$THOTH_WORKFLOW_TASK" = "download_package" ]; then
     exec python3 download_py_package.py
-elif [ "$THOTH_WORKFLOW_TASK" = "parse_solver_inputs"]; then
+elif [ "$THOTH_WORKFLOW_TASK" = "parse_solver_inputs" ]; then
     exec python3 parse_solver_inputs.py
 fi
