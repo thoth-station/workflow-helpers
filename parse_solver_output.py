aa4898051ccaf1354232771efd18470aa964822f
Bissenbay <bdauletb@redhat.com>
2020-09-23 12:31:35 -0400

    parse solver document

--- parse_solver_output.py
+++ parse_solver_output.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # workflow-helpers
-# Copyright(C) 2020 Francesco Murdaca
+# Copyright(C) 2020 Francesco Murdaca, Bissenbay Dauletbayev
 #
 # This program is free software: you can redistribute it and / or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,48 +15,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""This script run in a workflow task to parse solver inputs and produce inputs for Kafka message."""
+"""This script run in a workflow task to parse solver output and produce inputs for Kafka message."""
 
 import os
 import logging
 import json
 
-from thoth.workflow_helpers.common import retrieve_solver_service_version
+from thoth.workflow_helpers.common import retrieve_solver_document
 from thoth.workflow_helpers import __service_version__
 
 component_name = os.environ["THOTH_MESSAGING_COMPONENT_NAME"]
 document_path = os.environ["THOTH_SOLVER_DOCUMENT_PATH"]
 
-_LOGGER = logging.getLogger("thoth.parse_solver_inputs")
-_LOGGER.info("Thoth workflow-helpers task: parse_solver_inputs v%s", __service_version__)
+_LOGGER = logging.getLogger("thoth.parse_solver_output")
+_LOGGER.info("Thoth workflow-helpers task: parse_solver_output v%s", __service_version__)
 
 
-def parse_solver_inputs() -> None:
-    """Parse solver inputs and produce inputs for Kafka message."""
+def parse_solver_output() -> None:
+    """Parse solver output and produce inputs for Kafka message."""
     solver_name = os.environ["THOTH_SOLVER_NAME"]
-    package = os.environ["THOTH_SOLVER_PACKAGES"]
 
-    package_inputs = package.split("===")
-    package_name = package_inputs[0]
-    package_version = package_inputs[1]
-
-    _LOGGER.info(f"Solved package {package_name} in version: {package_version}")
-
-    solver_indexes = os.environ["THOTH_SOLVER_INDEXES"]
-    indexes = solver_indexes.split(",")
-
-    service_version = retrieve_solver_service_version(document_path)
+    solver_document = retrieve_solver_document(document_path)
+    service_version = solver_document["metadata"]["analyzer_version"]
 
     output_messages = []
 
-    for index_url in indexes:
-
+    for python_package_info in solver_document["result"]["tree"]:
         message_input = {
             "component_name": {"type": "str", "value": component_name},
             "service_version": {"type": "str", "value": service_version},
-            "package_name": {"type": "str", "value": package_name},
-            "package_version": {"type": "str", "value": package_version},
-            "index_url": {"type": "str", "value": index_url},
+            "package_name": {"type": "str", "value": python_package_info["package_name"]},
+            "package_version": {"type": "str", "value": python_package_info["package_version_requested"]},
+            "index_url": {"type": "str", "value": python_package_info["index_url"]},
             "solver": {"type": "str", "value": solver_name},
         }
 
@@ -70,4 +60,4 @@ def parse_solver_inputs() -> None:
 
 
 if __name__ == "__main__":
-    parse_solver_inputs()
+    parse_solver_output()
