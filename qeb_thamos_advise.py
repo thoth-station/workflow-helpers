93ac2ed2165b2b9ca7e78452aa43958abba6927b
sai <saisankargochhayat@gmail.com>
2020-10-06 17:02:50 -0400

    Qebhwt workflow now saves the adviser message

--- qeb_thamos_advise.py
+++ qeb_thamos_advise.py
@@ -23,7 +23,8 @@ import json
 import logging
 from pathlib import Path
 
-from thamos.lib import advise_here
+from thoth.python import Project
+from thamos.swagger_client import PythonStack
 from thamos.config import _Configuration
 from thamos.exceptions import NoRuntimeEnvironmentError
 from thamos.exceptions import NoRequirementsFormatError
@@ -38,6 +39,7 @@ from thoth.workflow_helpers import __service_version__
 
 _LOGGER = logging.getLogger("thoth.qebhwt")
 _LOGGER.info("Thoth workflow-helpers task: qebhwt v%s", __service_version__)
+__COMPOENENT_NAME__ = "Qeb-hwt"
 
 
 def _create_message_config_file_error(no_file: bool):
@@ -97,17 +99,24 @@ def qeb_hwt_thamos_advise() -> None:
         trigger_finished_webhook(exception_message=exception_message, has_error=True, error_type="MissingThothYamlFile")
         return
 
+    # Fetch recommedation type
     try:
-        analysis_id = advise_here(
-            nowait=True,
-            github_event_type=Configuration._GITHUB_EVENT_TYPE,
-            github_check_run_id=Configuration._GITHUB_CHECK_RUN_ID,
-            github_installation_id=Configuration._GITHUB_INSTALLATION_ID,
-            github_base_repo_url=Configuration._GITHUB_BASE_REPO_URL,
-            origin=Configuration._ORIGIN,
-            source_type=ThothAdviserIntegrationEnum.GITHUB_APP,
+        thoth_yaml_runtime_env = thoth_yaml_config.content.get("runtime_environments")
+        recommendation_type = thoth_yaml_runtime_env[0].get("recommendation_type") if thoth_yaml_config else "latest"
+
+        requirements_format = thoth_yaml_config.requirements_format
+        if requirements_format == "pipenv":
+            project = Project.from_files(without_pipfile_lock=not os.path.exists("Pipfile.lock"))
+        elif requirements_format in ("pip", "pip-tools", "pip-compile"):
+            project = Project.from_pip_compile_files(allow_without_lock=True)
+        else:
+            raise ValueError(f"Unknown configuration option for requirements format: {requirements_format!r}")
+
+        pipfile = project.pipfile.to_string()
+        pipfile_lock_str = project.pipfile_lock.to_string() if project.pipfile_lock else ""
+        application_stack = PythonStack(
+            requirements=pipfile, requirements_lock=pipfile_lock_str, requirements_format=requirements_format
         )
-        _LOGGER.info("Successfully submitted thamos advise call: %r", analysis_id)
     except Exception as exception:
         if isinstance(
             exception,
@@ -128,6 +137,31 @@ def qeb_hwt_thamos_advise() -> None:
             exception_message = json.loads(exception.body)["error"]  # type: ignore
 
         trigger_finished_webhook(exception_message=exception_message, has_error=True)
+        return
+
+    # The input for AdviserTriggerMessage if no exceptions were found
+    message_input = {
+        "component_name": {"type": "str", "value": __COMPOENENT_NAME__},
+        "service_version": {"type": "str", "value": __service_version__},
+        "application_stack": {"type": "Dict", "value": application_stack},
+        "recommendation_type": {"type": "str", "value": recommendation_type},
+        "github_event_type": {"type": "str", "value": Configuration._GITHUB_EVENT_TYPE},
+        "github_check_run_id": {"type": "int", "value": int(Configuration._GITHUB_CHECK_RUN_ID)},
+        "github_installation_id": {"type": "int", "value": int(Configuration._GITHUB_INSTALLATION_ID)},
+        "github_base_repo_url": {"type": "str", "value": Configuration._GITHUB_BASE_REPO_URL},
+        "origin": {"type": "str", "value": Configuration._ORIGIN},
+        "source_type": {"type": "str", "value": ThothAdviserIntegrationEnum.GITHUB_APP},
+    }
+
+    # We store the message to put in the output file here.
+    message_output = [{"topic_name": "thoth.adviser-trigger", "message_contents": message_input}]
+
+    # Store message to file that need to be sent.
+    with open(f"/mnt/workdir/messages_to_be_sent.json", "w") as json_file:
+        json.dump(message_output, json_file)
+
+    if message_output:
+        _LOGGER.info(f"Successfully stored file with messages to be sent!: {message_output}")
 
 
 if __name__ == "__main__":
