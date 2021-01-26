fa64dd843d5e45b13663583b37a971e28bb027fe
Francesco Murdaca <fmurdaca@redhat.com>
2021-01-26 12:03:38 +0100

    Make env public
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/workflow_helpers/common.py
+++ thoth/workflow_helpers/common.py
@@ -58,7 +58,7 @@ def store_messages(output_messages: list):
 def send_metrics():
     """Send metrics to pushgateway."""
     pushgateway_url = Configuration._THOTH_METRICS_PUSHGATEWAY_URL
-    deployment_name = Configuration._THOTH_DEPLOYMENT_NAME
+    deployment_name = Configuration.THOTH_DEPLOYMENT_NAME
 
     component_name = "workflow-helpers"
 
