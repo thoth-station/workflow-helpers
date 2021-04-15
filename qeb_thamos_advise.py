00fa31daa827ac4c8d2a6ed1dd8d43705fea7ace
Francesco Murdaca <fmurdaca@redhat.com>
2021-04-15 18:38:15 +0200

    no_wait=True
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- qeb_thamos_advise.py
+++ qeb_thamos_advise.py
@@ -130,6 +130,7 @@ def qeb_hwt_thamos_advise() -> None:
         origin=Configuration._ORIGIN,
         source_type=ThothAdviserIntegrationEnum.GITHUB_APP,
         no_static_analysis=False,
+        nowait=True,
     )
 
     _LOGGER.info(f"thamos advise response: {response}.")
