4500ae6ae0a4f9b6f6ccaef210ec53a3e421dd3a
Francesco Murdaca <fmurdaca@redhat.com>
2021-07-02 08:22:51 +0200

    Clean methods and README (#293)
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- app.sh
+++ app.sh
@@ -14,8 +14,6 @@ THOTH_WORKFLOW_TASK=${THOTH_WORKFLOW_TASK:?'THOTH_WORKFLOW_TASK is not selected!
 
 if [ "$THOTH_WORKFLOW_TASK" = "trigger_integration" ]; then
     exec python3 select_thoth_integration.py
-elif [ "$THOTH_WORKFLOW_TASK" = "qeb_hwt" ]; then
-    exec python3 qeb_thamos_advise.py
 elif [ "$THOTH_WORKFLOW_TASK" = "download_package" ]; then
     exec python3 download_py_package.py
 elif [ "$THOTH_WORKFLOW_TASK" = "parse_solver_output" ]; then
