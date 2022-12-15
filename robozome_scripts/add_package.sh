2702b6a2d51c375ee8099db3b48bc8b9c9f4bbc9
Christoph Görn <goern@redhat.com>
2022-12-15 11:39:31 +0100

    :sparkles: add add_package script for robozome
    Signed-off-by: Christoph Görn <goern@redhat.com>

---
+++ robozome_scripts/add_package.sh
@@ -0,0 +1,32 @@
+set -o allexport -o pipefail -ex
+
+# Pre-condition:
+# - Create the config file at location ${PAYLOAD_PATH}
+# - Clone the github.com/operate-first/apps repo at location ${WORKING_DIR}
+# - Working branch should be clean, checked out of upstream default branch.
+# - Set Environment variables: ORG_NAME (=thoth-station), SOURCE_REPO (=support), ISSUE_NUMBER
+#
+# If you want to run this script locally, you can set the following environment variables:
+# - create a data.yaml file in the root of the repo
+# - ISSUE_NUMBER=123 ./add_package.sh
+
+# get config from environment or default to data.yaml
+CONFIG=${PAYLOAD_PATH:-data.yaml}
+if [ -z "$WORKING_DIR" ]; then
+    REPO=$(pwd)
+else
+    REPO=${WORKING_DIR}/apps
+fi
+ORG_NAME=${ORG_NAME:-thoth-station}
+SOURCE_REPO=${SOURCE_REPO:-support}
+if [ -z "$ISSUE_NUMBER" ]; then
+    echo "ISSUE_NUMBER is not set"
+    exit 1
+fi
+
+# Unpack Config file, we will need these environment variables for the remainder of the steps
+PYTHON_PACKAGE_NAME=$(yq e .package_name ${CONFIG})
+PYTHON_INDEX_URL=$(yq e .index_url ${CONFIG})
+REASON=$(yq e .reason ${CONFIG})
+
+set -o allexport
